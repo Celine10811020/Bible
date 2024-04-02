81b07ccf045b5e54b88f2f54f7fb3f7260edb4b3
Celine10811020 <shinecloudhill@gmail.com>
2024-04-02 23:21:51 +0800

    add from git push

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 window.onload = function() {
-    // 获取所有的 thirdLayer 元素
-    var thirdLayers = document.querySelectorAll('.thirdLayer');
+    // 获取所有的 summary 元素
+    var summaries = document.querySelectorAll('.thirdLayer summary');
 
-    // 遍历每个 thirdLayer 元素
-    thirdLayers.forEach(function(thirdLayer) {
-        var summary = thirdLayer.querySelector('summary'); // 获取 thirdLayer 内部的 summary 元素
-        if (summary) {
-            var fileName = summary.textContent.trim(); // 获取 summary 的文本内容（文件名）
-            var filePath = './Notes/' + fileName + '.txt'; // 拼接文件路径
+    // 遍历每个 summary 元素
+    summaries.forEach(function(summary) {
+        var fileName = summary.textContent.trim(); // 获取 summary 的文本内容（文件名）
+        var filePath = 'https://celine10811020.github.io/Bible/Notes/' + encodeURIComponent(fileName) + '.txt'; // 替换为您的文本文件的 URL 地址
 
-            // 发起 HTTP GET 请求以获取文本文件内容
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', filePath);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    thirdLayer.innerHTML = xhr.responseText; // 将文本文件内容填充到对应的 div.thirdLayer 中
-                }
-            };
-            xhr.send();
-        }
+        // 发起 HTTP GET 请求以获取文本文件内容
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', filePath);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var forthLayer = summary.parentNode.querySelector('.forthLayer');
+                forthLayer.innerHTML = xhr.responseText; // 将文本文件内容填充到对应的 div.forthLayer 中
+            }
+        };
+        xhr.send();
     });
 };
